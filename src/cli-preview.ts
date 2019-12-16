158c9e85a62e891b6f8bd8907df2fe42ad1d4af6
spring-raining <harusamex.com@gmail.com>
2019-12-17 03:37:32 +0900

    Adopt subcommand system

---
+++ src/cli-preview.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// cli-preview will be called when we uses `preview` subcommand
+
+import path from 'path';
+import program from 'commander';
+import preview from './lib/preview';
+
+program
+  .name('vivliostyle preview')
+  .description('Open preview page and save PDF interactively')
+  .arguments('<input>')
+  .option(
+    '-b, --book',
+    `load document as book mode
+                             It can load multi-HTML documents such as an unzipped EPUB and a Web Publication.
+                             Please see also http://vivliostyle.github.io/vivliostyle.js/docs/en/`,
+  )
+  .option(
+    '--no-sandbox',
+    `launch chrome without sandbox (use this option to avoid ECONNREFUSED error)`,
+  )
+  .option(
+    '-r, --root <root_directory>',
+    `specify assets root path (default directory of input file)`,
+    undefined,
+  )
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+  program.help();
+}
+
+preview({
+  input: path.resolve(process.cwd(), program.args[0]),
+  rootDir: program.root && path.resolve(process.cwd(), program.root),
+  loadMode: program.book ? 'book' : 'document',
+  sandbox: program.sandbox,
+});
---
+++ src/cli-save.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+// cli-save will be called when we uses `save` subcommand
+
+import path from 'path';
+import program from 'commander';
+import save from './lib/save';
+
+const runningVivliostyleTimeout = 60 * 1000;
+
+program
+  .name('vivliostyle save')
+  .description('Launch headless Chrome and save PDF file')
+  .arguments('<input>')
+  .option(
+    '-b, --book',
+    `load document as book mode
+                             It can load multi-HTML documents such as an unzipped EPUB and a Web Publication.
+                             Please see also http://vivliostyle.github.io/vivliostyle.js/docs/en/`,
+  )
+  .option(
+    '--no-sandbox',
+    `launch chrome without sandbox (use this option to avoid ECONNREFUSED error)`,
+  )
+  .option(
+    '-r, --root <root_directory>',
+    `specify assets root path (default directory of input file)`,
+    undefined,
+  )
+  .option(
+    '-o, --output <output_file>',
+    `specify output file path (default output.pdf)`,
+    'output.pdf',
+  )
+  .option(
+    '-s, --size <size>',
+    `output pdf size (ex: 'A4' 'JIS-B5' '182mm,257mm' '8.5in,11in')`,
+  )
+  .option(
+    '-t, --timeout <time>',
+    `timeout times for waiting Vivliostyle process (default: 60s)`,
+    (val) =>
+      Number.isFinite(+val) && +val > 0
+        ? +val * 1000
+        : runningVivliostyleTimeout,
+  )
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+  program.help();
+}
+
+save({
+  input: path.resolve(process.cwd(), program.args[0]),
+  outputPath: path.resolve(process.cwd(), program.output),
+  size: program.size,
+  vivliostyleTimeout: program.timeout,
+  rootDir: program.root && path.resolve(process.cwd(), program.root),
+  loadMode: program.book ? 'book' : 'document',
+  sandbox: program.sandbox,
+});
