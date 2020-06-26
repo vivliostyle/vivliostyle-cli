2090464a0cac46fdd64bfd46dbf7ea69ed101d16
Yasuaki Uechi <y@uechi.io>
2020-06-26 13:22:44 +0900

    chore: update example

--- example/manuscripts/uetchy/epigraph.md
+++
@@ -1,6 +0,0 @@
----
-title: エピグラフ
-theme: 'https://raw.githubusercontent.com/vivliostyle/vivliostyle_doc/gh-pages/samples/gon/style.css'
----
-
-# Epigraph
--- example/vivliostyle.config.js
+++ example/vivliostyle.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  theme: '@vivliostyle/theme-bunko',
+  theme:
+    'https://raw.githubusercontent.com/youchan/viola-project/master/main.css',
   // theme: '../vivliostyle-theme-bunko',
   // theme: '../vivliostyle-theme-bunko/theme.css',
+  size: 'A4',
   entryContext: 'manuscripts',
-  entry: ['introduction.md', 'uetchy/epigraph.md'],
+  entry: [
+    'introduction.md',
+    {
+      path: 'fragments/chapter1.md',
+      theme: null,
+    },
+    'gingatetsudo.md',
+    'alice.md',
+  ],
+  // toc: './toc.html',
 };
