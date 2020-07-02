8e45ab1381b71d476568572f4ddf6fc01f5d2d41
Yasuaki Uechi <y@uechi.io>
2020-07-02 12:58:14 +0900

    fix(init): abort if config file exists

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -17,9 +17,9 @@ program
   .description('create vivliostyle config file')
   .option('--title <title>', 'title')
   .option('--author <author>', 'author')
-  .option('--language <language>', 'language')
-  .option('--size, -s', 'paper size')
-  .option('--theme <theme>', 'theme')
+  .option('--language, -l <language>', 'language')
+  .option('--size, -s <size>', 'paper size')
+  .option('--theme, -t <theme>', 'theme')
   .parse(process.argv);
 
 init({
@@ -31,32 +31,42 @@ init({
 }).catch(gracefulError);
 
 export default async function init(cliFlags: InitCliFlags) {
-  log(`Successfully created ${chalk.cyan('vivliostyle.config.js')}`);
-
   const vivliostyleConfigPath = path.join(
     process.cwd(),
     'vivliostyle.config.js',
   );
+
+  if (fs.existsSync(vivliostyleConfigPath)) {
+    return log(
+      `${chalk.yellow('vivliostyle.config.js already exists. aborting.')}`,
+    );
+  }
+
   // prettier-ignore
   const vivliostyleConfig = `module.exports = {
-  title: '${ cliFlags.title || 'Principia'}', // populated into \`manifest.json\`, default to \`title\` of the first entry or \`name\` in \`package.json\`.
-  author: '${cliFlags.author || 'Isaac Newton'}', // default to \`author\` in \`package.json\` or undefined
-  language: '${cliFlags.language || 'la'}', // default to \`en\`,
+  title: '${ cliFlags.title || 'Principia'}', // populated into 'manifest.json', default to 'title' of the first entry or 'name' in 'package.json'.
+  author: '${cliFlags.author || 'Isaac Newton'}', // default to 'author' in 'package.json' or undefined
+  language: '${cliFlags.language || 'la'}', // default to 'en'
   size: '${cliFlags.size || 'A4'}',
-  theme: '${cliFlags.theme || '@vivliostyle/theme-bunko'}', // .css or local dir or npm package. default to undefined
-  entry: [ // required
-    'introduction.md', // \`title\` is automatically guessed from the file (frontmatter > first heading)
-    {
-      path: 'epigraph.md',
-      title: 'おわりに', // title can be overwritten (entry > file),
-      theme: '@vivliostyle/theme-whatever' // theme can be set indivisually. default to root \`theme\`
-    },
-    'glossary.html' // html is also acceptable
-  ], // \`entry\` can be \`string\` or \`object\` if there's only single markdown file
-  // entryContext: '.', // default to '.' (relative to \`vivliostyle.config.js\`)
-  // toc: true, // whether generate and include toc.html or not (does not affect manifest.json), default to \`false\`. if \`string\` given, use it as a custom toc.html.
+  theme: '${cliFlags.theme || ''}', // .css or local dir or npm package. default to undefined
+  entry: [ // **required field**
+    // 'introduction.md', // 'title' is automatically guessed from the file (frontmatter > first heading)
+    // {
+    //   path: 'epigraph.md',
+    //   title: 'おわりに', // title can be overwritten (entry > file),
+    //   theme: '@vivliostyle/theme-whatever' // theme can be set indivisually. default to root 'theme'
+    // },
+    // 'glossary.html' // html is also acceptable
+  ], // 'entry' can be 'string' or 'object' if there's only single markdown file
+  // entryContext: './manuscripts', // default to '.' (relative to 'vivliostyle.config.js')
+  // outFile: './output.pdf', // path to generated pdf file. cannot be used with outDir.
+  // outDir: './output', // path to the directory where the generated pdf is located. filename is picked from 'title'. cannot be used with outFile.
+  // toc: true, // whether generate and include toc.html or not (does not affect manifest.json), default to 'false'. if 'string' given, use it as a custom toc.html.
+  // format: 'pdf', // reserved for future usage. default to 'pdf'.
+  // distDir: './build', // default to '.vivliostyle',
 };
 `;
 
   fs.writeFileSync(vivliostyleConfigPath, vivliostyleConfig);
+  log(`Successfully created ${chalk.cyan('vivliostyle.config.js')}`);
 }
