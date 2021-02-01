417df9441c26a027983976cb8720a00032dadd6b
spring-raining <harusamex.com@gmail.com>
2021-02-02 04:08:18 +0900

    chore: Show warnings if an unused cli flag is set

--- src/config.ts
+++ src/config.ts
@@ -413,6 +413,7 @@ export async function mergeConfig<T extends CliFlags>(
     ? await composeSingleInputConfig(commonOpts, cliFlags, config)
     : await composeProjectConfig(commonOpts, cliFlags, config, context);
   debug('parsedConfig', parsedConfig);
+  checkUnusedCliFlags(parsedConfig, cliFlags);
   return parsedConfig;
 }
 
@@ -624,4 +625,35 @@ async function composeProjectConfig<T extends CliFlags>(
       author: projectAuthor || '',
     },
   };
+}
+
+export function checkUnusedCliFlags<T extends CliFlags>(
+  config: MergedConfig,
+  cliFlags: T,
+) {
+  const unusedFlags: string[] = [];
+  if (!config.manifestPath) {
+    if (cliFlags.theme) {
+      unusedFlags.push('--theme');
+    }
+    if (cliFlags.title) {
+      unusedFlags.push('--title');
+    }
+    if (cliFlags.author) {
+      unusedFlags.push('--author');
+    }
+    if (cliFlags.language) {
+      unusedFlags.push('--language');
+    }
+  }
+  if (unusedFlags.length) {
+    log('\n');
+    unusedFlags.forEach((flag) => {
+      log(
+        `${chalk.bold.yellow(flag)}${chalk.bold.yellow(
+          ` flag seems to be set but the current export setting doesn't support this. This option will be ignored.`,
+        )}`,
+      );
+    });
+  }
 }
