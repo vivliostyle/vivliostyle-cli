0ba0730d7296d4ab6124a2184600dee13ee3d3c7
spring-raining <harusamex.com@gmail.com>
2021-02-07 00:59:40 +0900

    chore: Update init template

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -35,7 +35,7 @@ export default async function init(cliFlags: InitCliFlags) {
   const vivliostyleConfig = `module.exports = {
   title: '${ cliFlags.title || 'Principia'}', // populated into 'publication.json', default to 'title' of the first entry or 'name' in 'package.json'.
   author: '${cliFlags.author || 'Isaac Newton'}', // default to 'author' in 'package.json' or undefined
-  ${cliFlags.language ? '' : '// '}language: '${cliFlags.language || 'la'}', // default to 'en'
+  ${cliFlags.language ? '' : '// '}language: '${cliFlags.language || 'la'}',
   ${cliFlags.size ? '' : '// '}size: '${cliFlags.size || 'A4'}',
   ${cliFlags.theme ? '' : '// '}theme: '${cliFlags.theme || ''}', // .css or local dir or npm package. default to undefined
   entry: [ // **required field**
@@ -48,13 +48,14 @@ export default async function init(cliFlags: InitCliFlags) {
     // 'glossary.html' // html is also acceptable
   ], // 'entry' can be 'string' or 'object' if there's only single markdown file
   // entryContext: './manuscripts', // default to '.' (relative to 'vivliostyle.config.js')
-  // outputs: [ // path to generate draft file(s). default to '{title}.pdf'
+  // output: [ // path to generate draft file(s). default to '{title}.pdf'
   //   './output.pdf', // the output format will be inferred from the name.
   //   {
   //     path: './book',
   //     format: 'webpub',
   //   },
   // ],
+  // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
 };
