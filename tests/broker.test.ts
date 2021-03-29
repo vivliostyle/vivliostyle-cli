e2169f169c33bc64755653e34d51f841b12b42e6
spring-raining <harusamex.com@gmail.com>
2021-03-30 00:55:26 +0900

    chore: Update test case

--- tests/broker.test.ts
+++ tests/broker.test.ts
@@ -16,6 +16,7 @@ it('converts to valid broker url', async () => {
     url: getBrokerUrl({
       sourceIndex: '/absolute/path/to/something',
       singleDoc: true,
+      quick: true,
       outputSize: {
         width: '5in',
         height: '10in',
@@ -28,6 +29,6 @@ it('converts to valid broker url', async () => {
   };
   maskConfig(validOut2);
   expect(validOut2.url).toBe(
-    'file://__WORKSPACE__/node_modules/@vivliostyle/viewer/lib/index.html#src=file:///absolute/path/to/something&bookMode=false&renderAllPages=true&style=data:,#test>p::before{content:"エスケープ チェック";display:block}&userStyle=file://path/to/local/style/file/which/might/include/white space/%26/special#?character.css&userStyle=data:,/*<viewer>*/%40page%7Bsize%3A5in%2010in!important%3B%7D/*</viewer>*/',
+    'file://__WORKSPACE__/node_modules/@vivliostyle/viewer/lib/index.html#src=file:///absolute/path/to/something&bookMode=false&renderAllPages=false&style=data:,#test>p::before{content:"エスケープ チェック";display:block}&userStyle=file://path/to/local/style/file/which/might/include/white space/%26/special#?character.css&userStyle=data:,/*<viewer>*/%40page%7Bsize%3A5in%2010in!important%3B%7D/*</viewer>*/',
   );
 });
