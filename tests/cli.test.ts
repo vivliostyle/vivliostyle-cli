e61e45e5524f62a78f459e2aa56ae7f35952cc42
spring-raining <harusamex.com@gmail.com>
2023-07-13 23:17:39 +0900

    test: Remove obsoleted tests

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,14 +1,6 @@
 import execa from 'execa';
 import fileType from 'file-type';
 import fs from 'node:fs';
-import {
-  PDFCatalog,
-  PDFDict,
-  PDFDocument,
-  PDFHexString,
-  PDFName,
-  PDFNumber,
-} from 'pdf-lib';
 import path from 'upath';
 import { expect, it } from 'vitest';
 import packageJSON from '../package.json';
@@ -40,7 +32,7 @@ it('show version', async () => {
   expect(stdout).toContain(packageJSON.version);
 });
 
-it.only('generate pdf without errors', async () => {
+it('generate pdf without errors', async () => {
   const outputPath = path.join(localTmpDir, 'test.pdf');
   cleanUp(outputPath);
 
@@ -61,67 +53,4 @@ it.only('generate pdf without errors', async () => {
   // mimetype test
   const type = await fileType.fromFile(outputPath);
   expect(type!.mime).toEqual('application/pdf');
-}, 120000);
-
-it('generate press-ready pdf without errors', async () => {
-  const outputPath = path.join(localTmpDir, 'test-press-ready.pdf');
-  cleanUp(outputPath);
-
-  try {
-    const response = await vivliostyleCLI([
-      'build',
-      '-s',
-      'A4',
-      '-o',
-      outputPath,
-      '--press-ready',
-      fixtureFile,
-    ]);
-  } catch (err: any) {
-    throw err.stderr;
-  }
-
-  // mimetype test
-  const type = await fileType.fromFile(outputPath);
-  expect(type!.mime).toEqual('application/pdf');
-}, 120000);
-
-it('generates a PDF with metadata', async () => {
-  const outputPath = path.join(localTmpDir, 'test-metadata.pdf');
-
-  try {
-    const response = await vivliostyleCLI([
-      'build',
-      '-s',
-      'Letter',
-      '--title',
-      'Wood Engraving',
-      '-o',
-      outputPath,
-      fixtureFile,
-    ]);
-    expect(response.stdout).toContain('has been created');
-  } catch (err: any) {
-    throw err.stderr;
-  }
-
-  const bytes = fs.readFileSync(outputPath);
-  const document = await PDFDocument.load(bytes);
-
-  expect(document.getTitle()).toBe('Wood Engraving');
-
-  const catalog = document.context.lookup(
-    document.context.trailerInfo.Root,
-    PDFCatalog,
-  );
-
-  // Outlines
-  const outlines = catalog.lookup(PDFName.of('Outlines'), PDFDict);
-
-  const count = outlines.lookup(PDFName.of('Count'), PDFNumber);
-  expect(count.value()).toBe(1);
-
-  const intro = outlines.lookup(PDFName.of('First'), PDFDict);
-  const introTitle = intro.lookup(PDFName.of('Title'), PDFHexString);
-  expect(introTitle.sizeInBytes()).toBe(62);
 }, 120000);
