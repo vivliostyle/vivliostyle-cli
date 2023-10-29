7127c42a2a16e5c5c88a18ee5967e398c6b628aa
spring-raining <harusamex.com@gmail.com>
2023-10-29 16:23:46 +0900

    fix: Copy .htm files for webbook

--- src/output/webbook.ts
+++ src/output/webbook.ts
@@ -264,7 +264,7 @@ export async function copyWebPublicationAssets({
   const allFiles = await safeGlob(
     [
       `**/${upath.relative(input, manifestPath)}`,
-      '**/*.{html,html,css}',
+      '**/*.{html,htm,css}',
       ...includeAssets,
     ],
     {
